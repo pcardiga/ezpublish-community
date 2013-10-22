74009d96d6e3941fff049502664a017988253a25
MLoureiro <marcos.loureiro@ez.no>
2013-10-22 16:15:30 +0000

    Add user register implementation

--- src/EzSystems/BehatBundle/Features/Context/BrowserContext.php
+++ src/EzSystems/BehatBundle/Features/Context/BrowserContext.php
@@ -23,6 +23,69 @@ use Behat\Mink\Exception\UnsupportedDriverActionException as MinkUnsupportedDriv
  */
 class BrowserContext extends BaseFeatureContext
 {
+    /**
+     * Initializes context with parameters from behat.yml.
+     *
+     * @param array $parameters
+     */
+    public function __construct( array $parameters )
+    {
+        parent::__construct( $parameters );
+        $this->pageIdentifierMap += array( "home" => "/" );
+    }
+
+    /**
+     * Fill form will help to reduce the work for the tests where we got to fill
+     * every field (or simply some fields if want to test a (some) specific
+     * fields) without the need to specify every field
+     * @param array             $formData   This is the array data in $this->form["Context]["SpecificForm"]
+     * @param null|string|array $onlyListed Specifies the fields to be filled
+     *      null   = all fields
+     *      string = the only field
+     *      array  = list of the fields
+     * @return \Behat\Behat\Context\Step\When[]
+     */
+    public function fillForm( $formData, $onlyListed = null )
+    {
+        Assertion::assertNotNull( $formData, "Data for '$formData' form is empty" );
+
+        $newSteps = array();
+        // create the new steps When for adding the data to the form
+        foreach ( $formData as $field => $value )
+        {
+            // verify if this field is to be filled
+            if (
+                empty( $onlyListed )                // all fields
+                || $field === $onlyListed           // only specified field
+                || in_array( $field, $onlyListed )  // fields in list
+            )
+            {
+                // verify it is single value
+                if ( !is_array( $field ) )
+                    $newSteps[] = new Step\When( 'I fill "' . $field . '" with "' . $value . '"' );
+                // else create Gherkin Table
+                else {
+                    $fields = "|";
+                    foreach ( $value as $item )
+                    {
+                        // verify if its a single row table
+                        if ( !is_array( $item ) )
+                            $fields .= " $item |";
+                        // if its a multirow table create rows
+                        else {
+                            // fill row
+                            foreach ( $item as $last )
+                                $fields .= " $last |";
+
+                            $fields .= "\n|";
+                        }
+                    }
+                }
+            }
+        }
+
+        return $newSteps;
+    }
 
     /**
      * @Given /^(?:|I )am logged in as "([^"]*)" with password "([^"]*)"$/
@@ -39,8 +102,15 @@ class BrowserContext extends BaseFeatureContext
     }
 
     /**
-     * @Then /^(?:|I )am (?:at|on) the "([^"]*)(?:| page)"$/
+     * @Then /^(?:|I )am (?:at|on) the ([A-Za-z_-\d\s]*)page$/
+     * @Then /^(?:|I )see ([A-Za-z_-\d\s]*[^\s])(?:| )page$/
      * @Then /^(?:|I )see "([^"]*)" page$/
+     *
+     * All following sentences will be caught by this:
+     *      <sentence>                            => <$pageIdentifier>
+     *      I am on the homepage                  => "home"
+     *      see omg is this a real page           => "omg is this a real"
+     *      I see "really 3#£ weird symb*l" page  => "really 3#£ weird symb*l"
      */
     public function iAmOnThe( $pageIdentifier )
     {
@@ -56,7 +126,35 @@ class BrowserContext extends BaseFeatureContext
     }
 
     /**
-     * @Given /^(?:|I )click (?:on|at) "([^"]*)" link$/
+     * @Given /^I attach an image$/
+     */
+    public function iAttachAnImage()
+    {
+        $image = $this->getDummyContentFor( 'image' );
+
+        return new Step\When( 'I fill "image" with "' . $image . '"' );
+    }
+
+    /**
+     * @When /^(?:|I )click at "([^"]*)" button$/
+     * @When /^(?:|I )click at ([A-Za-z_-\d\s]*) button$/
+     */
+    public function iClickAtButton( $button )
+    {
+        $el = $this->getSession()->getPage()->findButton( $button );
+
+        // assert that button was found
+        Assertion::assertNotNull(
+            $el,
+            "Could not find '$button' button."
+        );
+
+        $el->click();
+    }
+
+    /**
+     * @Given /^(?:|I )click at "([^"]*)" link$/
+     * @When  /^(?:|I )click at ([A-Za-z_-\d\s]*) link$/
      *
      * Can also be used @When steps
      */
@@ -65,6 +163,45 @@ class BrowserContext extends BaseFeatureContext
         return array(
             new Step\When( "I follow \"{$link}\"" )
         );
+    }
+
+    /**
+     * @Then /^(?:|I )don\'t see "([^"]*)" button$/
+     */
+    public function iDonTSeeButton( $button )
+    {
+        $el = $this->getSession()->getPage()->findButton( $button );
+
+        if ( !empty( $el ) )
+            Assertion::assertNotEquals(
+                $button,
+                $el->getText(),
+                "Unexpected '$button' button is present"
+            );
+        else
+            Assertion::assertNull( $el );
+    }
+
+    /**
+     * @Then /^(?:|I )don\'t see image "([^"]*)"$/
+     */
+    public function iDonTSeeImage( $image )
+    {
+        throw new PendingException( "Image finder" );
+
+        $expected = $this->getFileByIdentifier( $image );
+        Assertion::assertFileExists( $expected, "Parameter '$expected' to be searched isn't a file" );
+
+        // iterate through all images checking
+        foreach ( $this->getSession()->getPage()->findAll( 'xpath', '//img' ) as $img )
+        {
+            $path = $this->locatePath( $img->getAttribute( "src" ) );
+            Assertion::assertFileNotEquals(
+                $expected,
+                $path,
+                "Unexpected '$image' image found"
+            );
+        }
     }
 
     /**
@@ -88,14 +225,151 @@ class BrowserContext extends BaseFeatureContext
     }
 
     /**
+     * @When /^(?:|I )fill a valid ([A-Za-z\s]+) form$/
+     */
+    public function iFillAValidForm( $form )
+    {
+        return $this->fillForm( $this->getFormData( $form ) );
+    }
+
+    /**
+     * @When /^(?:|I )fill ([A-Za-z\s]+) form with(?:|\:)$/
+     */
+    public function iFillFormWith( $form, TableNode $table )
+    {
+        $formData = $this->getFormData( $form );
+
+        $data = $this->convertTableToArrayOfData( $table, $formData );
+
+        // fill the form
+        return $this->fillForm( $data );
+    }
+
+    /**
+     * @When /^(?:|I )only fill(?:| the) form with(?:|\:)$/
+     */
+    public function iOnlyFillFormWith( TableNode $table )
+    {
+        $data = $this->convertTableToArrayOfData( $table );
+
+        // fill the form
+        return $this->fillForm( $data );
+    }
+
+
+    /**
+     * @When /^(?:|I )fill "([^"]*)" with "([^"]*)"$/
+     *         I fill <field> with <value>
+     *
+     * @todo Find a way to treat selection
+     * @todo Find a way to treat checkboxes
+     * @todo Find a way to treat multiple data ( ex: author, multi option ,... )
+     * @todo Find a way to treat with not specified values (ex: "A")
+     */
+    public function iFillWith( $field, $value )
+    {
+        // get page
+        $page = $this->getSession()->getPage();
+
+        $fieldElement = $page->find( 'xpath', "//input[contains( @id, '$field' )]" );
+
+        // assert that the field was found
+        Assertion::assertNotNull(
+            $fieldElement,
+            "Could not find '$field' field."
+        );
+
+        // check if data is in fact an identifier (alias)
+        if ( $this->isSingleCharacterIdentifier( $value ) )
+            $value = $this->getDummyContentFor( $field, $value );
+
+        // insert following data
+        if ( strtolower( $fieldElement->getAttribute( 'type' ) ) === 'file' )
+        {
+            Assertion::assertFileExists( $value, "File '$value' not found (for field '$field')" );
+            $fieldElement->attachFile( $value );
+        }
+        else
+            $fieldElement->setValue( $value );
+    }
+
+    /**
      * @Given /^(?:|I )am (?:at|on) (?:|the )"([^"]*)" page$/
      * @When  /^(?:|I )go to (?:|the )"([^"]*)"(?:| page)$/
+     * @When  /^(?:|I )go to (?:|the )([A-Za-z_-\d\s]*)(?:| page)$/
      */
     public function iGoToThe( $pageIdentifier )
     {
         return array(
             new Step\When( 'I am on "' . $this->getPathByPageIdentifier( $pageIdentifier ) . '"' ),
         );
+    }
+
+    /**
+     * @Then /^(?:|I )see "([^"]*)" button with attributes(?:|\:)$/
+     */
+    public function iSeeButtonWithAttributes( $button, TableNode $table )
+    {
+        $el = $this->getSession()->getPage()->findButton( $button );
+
+        // assert that button was found
+        Assertion::assertNotNull(
+            $el,
+            "Could not find '$button' button."
+        );
+
+        Assertion::assertEquals(
+            $button,
+            $el->getText(),
+            "Failed asserting that '$button' is equal to '{$el->getText()}"
+        );
+
+        $rows = $table->getRows();
+        array_shift( $rows );   // this is needed to take the first row ( readability only )
+        foreach ( $rows as $attribute => $value )
+        {
+            // assert that attribute was found
+            Assertion::assertNotNull(
+                $el->getAttribute( $attribute ),
+                "Couldn't find '$attribute' attribute."
+            );
+
+            Assertion::assertEquals(
+                $value,
+                $el->getAttribute( $attribute ),
+                "Value '$value' of '$attribute' attribute doesn't match '{$el->getAttribute( $attribute )}'"
+            );
+        }
+    }
+
+    /**
+     * @Then /^(?:|I )see ["'](.+)["'] (?:warning|error)(?:| message)$/
+     */
+    public function iSeeError( $error )
+    {
+        $escapedText = $this->getSession()->getSelectorsHandler()->xpathLiteral( $error );
+
+        $page = $this->getSession()->getPage();
+        $tmp = array_merge(
+            $page->findAll( 'css', 'div.warning' ), // warnings
+            $page->findAll( 'css', 'div.error' )    // errors
+        );
+        foreach ( $tmp as $el )
+        {
+            $aux = $el->find( 'named', array( 'content', $escapedText ) );
+            if ( !empty( $aux ) )
+            {
+                Assertion::assertContains(
+                    $error,
+                    $aux->getText(),
+                    "Couldn't find '$error' error message in '{$aux->getText()}'"
+                );
+                return;
+            }
+        }
+
+        // if not found throw an failed assertion
+        Assertion::fail( "Couldn't find '$error' error message" );
     }
 
     /**
@@ -139,6 +413,34 @@ class BrowserContext extends BaseFeatureContext
     }
 
     /**
+     * @Then /^(?:|I )see ["'](.+)["'] message$/
+     *
+     * @todo Find if this messages go into a specific tag.class
+     */
+    public function iSeeMessage( $message )
+    {
+        $el = $this->getSession()->getPage()->find(
+            "named",
+            array(
+                "content",
+                $this->getSession()->getSelectorsHandler()->xpathLiteral( $message )
+            )
+        );
+
+        // assert that message was found
+        Assertion::assertNotNull(
+            $el,
+            "Could not find '$message' message."
+        );
+
+        Assertion::assertContains(
+            $message,
+            $el->getText(),
+            "Couldn't find '$message' message in '{$el->getText()}'"
+        );
+    }
+
+    /**
      * @Given /^(?:|I )see search (\d+) result$/
      */
     public function iSeeSearchResults( $arg1 )
@@ -167,7 +469,7 @@ class BrowserContext extends BaseFeatureContext
      *      ...
      *  );
      *
-     * @todo verify if the links are for objects 
+     * @todo verify if the links are for objects
      * @todo check if it has a different url alias
      * @todo check "parent" node
      */
--- src/EzSystems/BehatBundle/Features/Context/FeatureContext.php
+++ src/EzSystems/BehatBundle/Features/Context/FeatureContext.php
@@ -11,6 +11,7 @@
 
 namespace EzSystems\BehatBundle\Features\Context;
 
+use Behat\Gherkin\Node\TableNode;
 use Behat\MinkExtension\Context\MinkContext;
 use Behat\Symfony2Extension\Context\KernelAwareInterface;
 use Behat\Behat\Exception\PendingException;
@@ -34,6 +35,11 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
     protected $parameters;
 
     /**
+     * @var EzSystems\BehatBundle\Features\Context\Helpers\ContentManager
+     */
+    protected $contentManager;
+
+    /**
      * @var array Array to map identifier to urls, should be set by child classes.
      */
     protected $pageIdentifierMap = array();
@@ -52,6 +58,20 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
      * @var array This will have a ( identifier => array )
      */
     public $mainAttributes = array();
+
+    /**
+     * Valid Forms, should be set by child classes.
+     * Should be defined by key => array( $data, ... )
+     * $data = array(
+     *      "field1" => "value1",
+     *      "field2" => array( "if, "it", "has", "multiple", "values" ),
+     *      ...
+     *      "fieldN" => "valueN",
+     * );
+     *
+     * @var array
+     */
+    protected $forms = array();
 
     /**
      * @var string
@@ -77,6 +97,79 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
     public function setKernel( KernelInterface $kernel )
     {
         $this->kernel = $kernel;
+    }
+
+    /**
+     * This function will convert Gherkin tables into structure array of data
+     *
+     * @param \Behat\Gherkin\Node\TableNode|array   $table The Gherkin table to extract the values
+     * @param null|array                            $data  If there are values to be concatenated/updated
+     *
+     * @return false|array
+     *
+     * the returned array should look like:
+     *      $data = array(
+     *          "field1" => "single value 1",
+     *          "field2" => "single value 2",
+     *          "field3" => array( "multiple", "value", "here"),
+     *          ...
+     *      );
+     */
+    public function convertTableToArrayOfData( TableNode $table, $data = null )
+    {
+        if( empty( $data ) )
+            $data = array();
+
+        // prepare given data
+        $i = 0;
+        $rows = $table->getRows();
+        array_shift( $rows );   // this is needed to take the first row ( readability only )
+        foreach ( $rows as $row )
+        {
+            $count = count( array_filter( $row ) );
+            // check if the field is supposed to be empty
+            // or it simply has only 1 element
+            if (
+                $count == 1
+                && count( $row )
+                && !method_exists( $table, "getCleanRows" )
+            )
+            {
+                $count = 2;
+            }
+
+            $key = $row[0];
+            switch( $count ){
+            // case 1 is for the cases where there is an Examples table and it
+            // gets the values from there, so the field name/id shold be on the
+            // examples table (ex: "| <field_name> |")
+            case 1:
+                $value = $key;
+                $aux = $table->getCleanRows();
+                if ( count( $aux ) === count( array_keys( $table ) ) )
+                    $k = $i;
+                else
+                    $k = $i +1;
+
+                $key = str_replace( array( '<', '>' ), array( '', '' ), $aux[$k][0] );
+                break;
+
+            // case 2 is the most simple case where "| field1 | as value 1 |"
+            case 2:
+                $value = $row[1];
+                break;
+
+            // this is for the cases where there are several values for the same
+            // field (ex: author) and the gherkin table should look like
+            default: $value = array_slice( $row, 1 );
+                break;
+            }
+            $data[$key] = $value;
+            $i++;
+        }
+
+        // if its empty return false otherwise return the array with data
+        return empty( $data )? false : $data;
     }
 
     /**
@@ -136,6 +229,23 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
     }
 
     /**
+     * Returns valid form data
+     *
+     * @param  string $form Name of the intended form
+     * @return null|array
+     *
+     * @throws PendingException When the form is not setted
+     */
+    public function getFormData( $form )
+    {
+        $form = strtolower( $form );
+        if ( isset( $this->forms[$form] ) )
+            return $this->forms[$form];
+
+        throw new PendingException( "Data for '$form' form no defined" );
+    }
+
+    /**
      * With this function we get a centralized way to define what are the possible
      * tags for a type of data and return them as a xpath search
      *
@@ -169,13 +279,13 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
      *
      * @return string
      */
-    public function concatTagsWithXpath( array $tags, $xpath = null, $pre = null )
+    public function concatTagsWithXpath( array $tags, $xpath = null )
     {
         $finalXpath = "";
-        for ( $i = 0 ; !empty( $tags[$i] ) ; $i++ )
+        for ( $i = 0; !empty( $tags[$i] ); $i++ )
         {
             $finalXpath .= "//{$tags[$i]}$xpath";
-            if ( !empty($tags[$i+1]) )
+            if ( !empty($tags[$i + 1]) )
                 $finalXpath .= " | ";
         }
 
@@ -191,6 +301,65 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
     public function literal( $text )
     {
         return $this->getSession()->getSelectorsHandler()->xpathLiteral( $text );
+    }
+
+    /**
+     * Verify if a value is a single character identifier
+     *
+     * @param  mixed  $value The value to test if is identifier
+     *
+     * @return boolean
+     */
+    public function isSingleCharacterIdentifier( $value )
+    {
+        return is_string( $value ) && strlen( $value ) == 1 && $value >= 'A' && $value <= 'Z';
+    }
+
+    /**
+     * This will cover the dummy data
+     *
+     * @param  string      $type       This is for specifying what kind of dummy data to return/store
+     * @param  null|string $identifier This is the identifier for the content holder
+     */
+    public function getDummyContentFor( $type, $identifier = null )
+    {
+        $data = null;
+        // get/make the intended data
+        switch( $type ) {
+        case 'integer':
+        case 'ezinteger':
+            $data = rand( 1000, 99999999 );
+            break;
+
+        case 'float':
+        case 'ezfloat':
+            $data = rand( 1000, 99999999 ) / rand( 10, 500 );
+            break;
+
+        case 'identifier':
+            $data = "id" . rand( 1000, 9999 ) . "RND";
+            break;
+
+        case 'text':
+        case 'eztext':
+        case 'string':
+        case 'ezstring':
+            $data = "This is a text string with some rand data " . rand( 1000, 9999 );
+            break;
+
+        case 'image':
+            $data = __DIR__ . "/_fixtures/image.png";
+            break;
+
+        default:
+            throw new PendingException( "Define dummy data for '$type' type" );
+        }
+
+        // if a identifier was passed is to create/update a key on content holder
+        if( $identifier )
+            $this->contentHolder[$identifier] = $data;
+
+        return $data;
     }
 
     /**
@@ -225,6 +394,14 @@ class FeatureContext extends MinkContext implements KernelAwareInterface
         }
 
         return $url;
+    }
+
+    /**
+     * @Given /^I got "([^"]*)" disabled$/
+     */
+    public function iGotDisabled( $setting )
+    {
+        throw new PendingException( "need system management" );
     }
 
     /**
